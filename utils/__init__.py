067ccdd2701b8a343e994f6547bd9f9e6a75b281
bosenda20000 <11230013@wfsh.tp.edu.tw>
2025-03-23 17:48:56 +0800

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,6 @@
 from .transform import plot_waveform, plot_frequency_spectrum
 from .compare_math import compare_audio
-from .compare_photo import plot_waveform, plot_spectrogram
+from .compare_photo import plot_waveform_compare, plot_spectrogram_compare
 from .encode import generate_encryption_id, embed_watermark, speed_up_audio
 from .decode import extract_watermark, remove_watermark
 from .save_file import save_photo, save_audio
